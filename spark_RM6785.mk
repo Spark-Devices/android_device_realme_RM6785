8ba63dda414f2b7c7af22bf4ce32148cf9f8400e
RahulGorai0206 <rahulgorai0206@gmail.com>
2022-06-17 20:09:08 +0530

    RM6785:FP:Update Build Fingerprint To Raven june

--- spark_RM6785.mk
+++ spark_RM6785.mk
@@ -23,6 +23,10 @@ PRODUCT_BRAND := realme
 PRODUCT_MODEL := RM6785
 PRODUCT_MANUFACTURER := realme
 
-BUILD_FINGERPRINT := "realme/RMX2001/RMX2001L1:10/QP1A.190711.020/1594211000:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="raven-user 12 SQ3A.220605.009.B1 8650216 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ3A.220605.009.B1/8650216:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
