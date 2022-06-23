8b5057ac9f35216bc1c8b977ecb78848a80a2184
RahulGorai0206 <rahulgorai0206@gmail.com>
2022-06-24 01:16:29 +0530

    RM6785:Project-Zephyrus bringup

--- RM6785.mk
+++ RM6785.mk
@@ -1,7 +1,17 @@
 #
-# Copyright (C) 2022 Nusantara Android Development
+# Copyright (C) 2022 Android Open Source Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 # Inherit from those products. Most specific first.
@@ -12,12 +22,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/RM6785/device.mk)
 
 # Inherit some common AOSP stuff.
-TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_BOOT_ANIMATION_RES := 1080
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Project-Zephyrus Stuff.
+WITH_GMS := true
+WITH_LAWNCHAIR := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_RM6785
+PRODUCT_NAME := RM6785
 PRODUCT_DEVICE := RM6785
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RM6785
